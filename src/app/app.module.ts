a800e9f0ea6a2d4590ec43c5437190c0d7c5ba4f
Ivan Mateus Nieradka <ivan.nieradka@hopelingerie.com.br>
2023-04-20 11:40:52 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
 
-    
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,32 +1,19 @@
-import { Component,OnInit  } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 declare var bootstrap: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-  
-
 export class HomeComponent implements OnInit {
-
-
-
   ngOnInit() {
-    let offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
+    let offcanvasElementList = [].slice.call(
+      document.querySelectorAll('.offcanvas')
+    );
     let offcanvasList = offcanvasElementList.map(function (offcanvasEl: any) {
-      return new bootstrap.Offcanvas(offcanvasEl)
+      return new bootstrap.Offcanvas(offcanvasEl);
     });
   }
-
-
-  openNav() {
-    let offcanvasElement = document.getElementById('navbarNav');
-    let offcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement);
-    offcanvas.show();
-  }
-
-
 }
